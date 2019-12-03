be2015f110bc834c6907d0d2d6d1eeb43c984e50
wenbin <1147512352@qq.com>
2019-12-03 21:42:44 +0800

    'b'

--- src/hello.tsx
+++ src/hello.tsx
@@ -17,7 +17,7 @@ export default class Hello extends PureComponent{
         }
     }
     componentDidMount(){
-        console.log(this.props)
+        console.log(this.props);
         this._getName({name:'bin'});
     }
     _getName(params:_name){
